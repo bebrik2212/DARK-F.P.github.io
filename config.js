5ed6095b5378a59220363d8789a73c223b3d4d66
bebrik2212 <1488oleg22@gmail.com>
2026-08-05 00:05:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBa9NWi5FfpmAx0ExJh1fJ3b1ipUEEBRxU",
-  authDomain: "dark-fortport.firebaseapp.com",
-  projectId: "dark-fortport",
+  apiKey: "AIzaSyBa9NWi5FfpmAx0ExJh1fJ3b1ipUEEBRxU"
+authDomain: "dark-fortport.firebaseapp.com"
+projectId: "dark-fortport"
   storageBucket: "dark-fortport.firebasestorage.app",
   messagingSenderId: "3814531503",
   appId: "1:3814531503:web:a8200e1f337935a3530f5a",

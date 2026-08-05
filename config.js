64a09f97b0182ffce5c3719b290dbcf931f63045
bebrik2212 <1488oleg22@gmail.com>
2026-08-05 16:47:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Firebase конфигурация
+// config.js - Firebase конфигурация
 const firebaseConfig = {
     apiKey: "AIzaSyBa9NWi5FpmAx6ExJh1fJ3b1ipUEEBRxU",
     authDomain: "dark-fortport.firebaseapp.com",
@@ -8,5 +8,11 @@ const firebaseConfig = {
     appId: "1:3814531503:web:a8200e1f337935a3530f5a"
 };
 
+// Инициализация Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+// Включаем оффлайн режим
+db.enablePersistence()
+    .then(() => console.log('🔥 Offline mode enabled'))
+    .catch((err) => console.warn('Offline mode error:', err));
